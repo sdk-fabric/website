cddb4c8033218dc894b5256e1126da575648f3f0
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-05-01 11:32:16 +0200

    update design

--- resources/template/faq.php
+++ resources/template/faq.php
@@ -1,11 +1,15 @@
 
 <?php include __DIR__ . '/inc/header.php'; ?>
 
-<h1 class="display-4 mt-5">FAQ</h1>
-<?php foreach($questions as $question): ?>
-  <hr>
-  <h3><?php echo $question->question; ?></h3>
-  <div><?php echo $question->answer; ?></div>
-<?php endforeach; ?>
+<h1 class="display-4 mt-5 mb-3 text-center">FAQ</h1>
+
+<ul class="lead">
+  <?php foreach($questions as $question): ?>
+  <li>
+    <p><b><?php echo $question->question; ?></b></p>
+    <div><?php echo $question->answer; ?></div>
+  </li>
+  <?php endforeach; ?>
+</ul>
 
 <?php include __DIR__ . '/inc/footer.php'; ?>
